fd5b8b6b27ee0c6e42a75d7c2a9f26c51cfd792e
Jesuraj77 <isysraju2001@gmail.com>
2020-03-02 12:33:20 +0530

    added

---
+++ Eclipse Che Link for Core 3.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eclipse_Che_Link_for_Core_3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
